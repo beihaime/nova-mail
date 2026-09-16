d27c2dfaf8a3adfc6f032d1694a36e12613492a0
beihaime <beihaime@outlook.com>
2026-09-17 02:30:50 +0800

    fix: prevent SQL injection in public addUser

--- mail-worker/src/service/public-service.js
+++ mail-worker/src/service/public-service.js
@@ -19,28 +19,28 @@ const publicService = {
 
 	async emailList(c, params) {
 
-		let { toEmail, content, subject, sendName, sendEmail, timeSort, num, size, type , isDel } = params
+		let { toEmail, content, subject, sendName, sendEmail, timeSort, num, size, type, isDel } = params;
 
 		const query = orm(c).select({
-				emailId: email.emailId,
-				sendEmail: email.sendEmail,
-				sendName: email.name,
-				subject: email.subject,
-				toEmail: email.toEmail,
-				toName: email.toName,
-				type: email.type,
-				createTime: email.createTime,
-				content: email.content,
-				text: email.text,
-				isDel: email.isDel,
-		}).from(email)
+			emailId: email.emailId,
+			sendEmail: email.sendEmail,
+			sendName: email.name,
+			subject: email.subject,
+			toEmail: email.toEmail,
+			toName: email.toName,
+			type: email.type,
+			createTime: email.createTime,
+			content: email.content,
+			text: email.text,
+			isDel: email.isDel,
+		}).from(email);
 
 		if (!size) {
-			size = 20
+			size = 20;
 		}
 
 		if (!num) {
-			num = 1
+			num = 1;
 		}
 
 		size = Number(size);
@@ -48,40 +48,40 @@ const publicService = {
 
 		num = (num - 1) * size;
 
-		let conditions = []
+		let conditions = [];
 
 		if (toEmail) {
-			conditions.push(sql`${email.toEmail} COLLATE NOCASE LIKE ${toEmail}`)
+			conditions.push(sql`${email.toEmail} COLLATE NOCASE LIKE ${toEmail}`);
 		}
 
 		if (sendEmail) {
-			conditions.push(sql`${email.sendEmail} COLLATE NOCASE LIKE ${sendEmail}`)
+			conditions.push(sql`${email.sendEmail} COLLATE NOCASE LIKE ${sendEmail}`);
 		}
 
 		if (sendName) {
-			conditions.push(sql`${email.name} COLLATE NOCASE LIKE ${sendName}`)
+			conditions.push(sql`${email.name} COLLATE NOCASE LIKE ${sendName}`);
 		}
 
 		if (subject) {
-			conditions.push(sql`${email.subject} COLLATE NOCASE LIKE ${subject}`)
+			conditions.push(sql`${email.subject} COLLATE NOCASE LIKE ${subject}`);
 		}
 
 		if (content) {
-			conditions.push(sql`${email.content} COLLATE NOCASE LIKE ${content}`)
+			conditions.push(sql`${email.content} COLLATE NOCASE LIKE ${content}`);
 		}
 
 		if (type || type === 0) {
-			conditions.push(eq(email.type, type))
+			conditions.push(eq(email.type, type));
 		}
 
 		if (isDel || isDel === 0) {
-			conditions.push(eq(email.isDel, isDel))
+			conditions.push(eq(email.isDel, isDel));
 		}
 
 		if (conditions.length === 1) {
-			query.where(...conditions)
+			query.where(...conditions);
 		} else if (conditions.length > 1) {
-			query.where(and(...conditions))
+			query.where(and(...conditions));
 		}
 
 		if (timeSort === 'asc') {
@@ -91,7 +91,6 @@ const publicService = {
 		}
 
 		return query.limit(size).offset(num);
-
 	},
 
 	async addUser(c, params) {
@@ -116,7 +115,6 @@ const publicService = {
 			emailRow.hash = hash;
 		}
 
-
 		const activeIp = reqUtils.getIp(c);
 		const { os, browser, device } = reqUtils.getUserAgent(c);
 		const activeTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
@@ -135,45 +133,63 @@ const publicService = {
 				type = roleRow ? roleRow.roleId : type;
 			}
 
-			const userSql = `INSERT INTO user (email, password, salt, type, os, browser, active_ip, create_ip, device, active_time, create_time)
-			VALUES ('${email}', '${hash}', '${salt}', '${type}', '${os}', '${browser}', '${activeIp}', '${activeIp}', '${device}', '${activeTime}', '${activeTime}')`
+			const userName = emailUtils.getName(email);
 
-			const accountSql = `INSERT INTO account (email, name, user_id)
-			VALUES ('${email}', '${emailUtils.getName(email)}', 0);`;
+			userList.push(
+				c.env.db.prepare(
+					`INSERT INTO user (email, password, salt, type, os, browser, active_ip, create_ip, device, active_time, create_time)
+					 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+				).bind(
+					email,
+					hash,
+					salt,
+					type,
+					os,
+					browser,
+					activeIp,
+					activeIp,
+					device,
+					activeTime,
+					activeTime
+				)
+			);
 
-			userList.push(c.env.db.prepare(userSql));
-			userList.push(c.env.db.prepare(accountSql));
-
+			userList.push(
+				c.env.db.prepare(
+					`INSERT INTO account (email, name, user_id) VALUES (?, ?, 0)`
+				).bind(email, userName)
+			);
 		}
 
-		userList.push(c.env.db.prepare(`UPDATE account SET user_id = (SELECT user_id FROM user WHERE user.email = account.email) WHERE user_id = 0;`))
+		userList.push(
+			c.env.db.prepare(
+				`UPDATE account SET user_id = (SELECT user_id FROM user WHERE user.email = account.email) WHERE user_id = 0`
+			)
+		);
 
 		try {
 			await c.env.db.batch(userList);
 		} catch (e) {
-			if(e.message.includes('SQLITE_CONSTRAINT')) {
-				throw new BizError(t('emailExistDatabase'))
+			if (e.message.includes('SQLITE_CONSTRAINT')) {
+				throw new BizError(t('emailExistDatabase'));
 			} else {
-				throw e
+				throw e;
 			}
 		}
-
 	},
 
 	async genToken(c, params) {
-
-		await this.verifyUser(c, params)
+		await this.verifyUser(c, params);
 
 		const uuid = uuidv4();
 
 		await c.env.kv.put(KvConst.PUBLIC_KEY, uuid);
 
-		return {token: uuid}
+		return { token: uuid };
 	},
 
 	async verifyUser(c, params) {
-
-		const { email, password } = params
+		const { email, password } = params;
 
 		const userRow = await userService.selectByEmailIncludeDel(c, email);
 
@@ -189,7 +205,6 @@ const publicService = {
 			throw new BizError(t('IncorrectPwd'));
 		}
 	}
+};
 
-}
-
-export default publicService
+export default publicService;
